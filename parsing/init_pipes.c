ada3fc7b4b412a73c06475b74be7a5201441dba8
Jean-Baptiste Lepany <jlepany@f2r6s14.clusters.42paris.fr>
2025-05-16 13:14:30 +0200

    made sure if redirection output is alread set, pipes will not intefere with it

--- parsing/init_pipes.c
+++ parsing/init_pipes.c
@@ -6,7 +6,7 @@
 /*   By: jlepany <jlepany@student.42,fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 00:32:04 by jlepany           #+#    #+#             */
-/*   Updated: 2025/05/15 14:05:06 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/16 12:43:05 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 int	give_pipe(t_env *mini_env, t_shell **command, int i)
 {
-	t_io	*new_io;
-
-	new_io = init_new_io(mini_env, *command, 0);
 	if ((*command)->command)
 	{
-		new_io->io_mode = 3;
+		if (!(*command)->first_output)
+		{
+			(*command)->first_output = init_new_io(mini_env, *command, 0);
+			(*command)->first_output->io_mode = 3;
+		}
 		(*command)->output++;
 		*command = (*command)->next_command;
-		new_io = init_new_io(mini_env, *command, 1);
-		new_io->io_mode = 3;
+		(*command)->first_input = init_new_io(mini_env, *command, 1);
+		(*command)->first_input->io_mode = 3;
 	}
 	return (i + 1);
 }

69fc04dc341a8df712068cf8bef8c605417112a0
Jean-Baptiste Lepany <jlepany@f2r5s14.clusters.42paris.fr>
2025-05-19 14:59:11 +0200

    solving pb for never ending commands during pipe

--- parsing/init_pipes.c
+++ parsing/init_pipes.c
@@ -6,7 +6,7 @@
 /*   By: jlepany <jlepany@student.42,fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 00:32:04 by jlepany           #+#    #+#             */
-/*   Updated: 2025/05/16 12:43:05 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/19 14:46:10 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	give_pipe(t_env *mini_env, t_shell **command, int i)
 		}
 		(*command)->output++;
 		*command = (*command)->next_command;
+		(*command)->input++;
 		(*command)->first_input = init_new_io(mini_env, *command, 1);
 		(*command)->first_input->io_mode = 3;
 	}

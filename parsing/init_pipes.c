f1735aaa0d1d1f4c1e259dda00e7256eed1d5311
jblep <jlepany@outlook.fr>
2025-05-19 18:01:21 -0400

    pipe was not added when another file was taken into output. but it should. because maybe another file will want to read from this pipe.

--- parsing/init_pipes.c
+++ parsing/init_pipes.c
@@ -6,7 +6,7 @@
 /*   By: jlepany <jlepany@student.42,fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 00:32:04 by jlepany           #+#    #+#             */
-/*   Updated: 2025/05/19 14:46:10 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/19 17:57:58 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@ int	give_pipe(t_env *mini_env, t_shell **command, int i)
 {
 	if ((*command)->command)
 	{
-		if (!(*command)->first_output)
-		{
-			(*command)->first_output = init_new_io(mini_env, *command, 0);
-			(*command)->first_output->io_mode = 3;
-		}
+		(*command)->first_output = init_new_io(mini_env, *command, 0);
+		(*command)->first_output->io_mode = 3;
 		(*command)->output++;
 		*command = (*command)->next_command;
 		(*command)->input++;

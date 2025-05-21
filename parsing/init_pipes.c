d182d121066914cebf790fb198d586292c29f213
jblep <jlepany@outlook.fr>
2025-05-21 16:09:29 -0400

    pipe will still be created if there is no command but only an input/output. It is now conform to bash

--- parsing/init_pipes.c
+++ parsing/init_pipes.c
@@ -6,7 +6,7 @@
 /*   By: jlepany <jlepany@student.42,fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 00:32:04 by jlepany           #+#    #+#             */
-/*   Updated: 2025/05/19 18:57:48 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/21 16:07:11 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int	give_pipe(t_env *mini_env, t_shell **command, int i)
 {
 	t_io	*tmp;
 
-	if ((*command)->command)
-	{
-		tmp = init_new_io(mini_env, *command, 0);
-		tmp->io_mode = 3;
-		(*command)->output++;
-		*command = (*command)->next_command;
-		(*command)->input++;
-		(*command)->first_input = init_new_io(mini_env, *command, 1);
-		(*command)->first_input->io_mode = 3;
-	}
+	tmp = init_new_io(mini_env, *command, 0);
+	tmp->io_mode = 3;
+	(*command)->output++;
+	*command = (*command)->next_command;
+	(*command)->input++;
+	(*command)->first_input = init_new_io(mini_env, *command, 1);
+	(*command)->first_input->io_mode = 3;
 	return (i + 1);
 }

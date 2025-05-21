f2bff10932904d0b0426be4a7decd246b10e6398
Jean-Baptiste Lepany <jlepany@f1r5s15.clusters.42paris.fr>
2025-05-21 16:52:50 +0200

    added some new function to initialize garbage collector and clean it too

--- execution/is_special_buildin.c
+++ execution/is_special_buildin.c
@@ -6,11 +6,22 @@
 /*   By: apesic <apesicstudent.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 11:04:32 by apesic            #+#    #+#             */
-/*   Updated: 2025/05/19 13:11:09 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/21 16:48:37 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
+
+int	clean_garbage(t_leak *garbage)
+{
+	free_double_char(garbage->envp);
+	free_double_char(garbage->arg);
+	if (garbage->path)
+		free_double_char(garbage->path);
+	free(garbage->child_id);
+	free(garbage);
+	return (0);
+}
 
 int	decrement_return_z(int *to_decrement)
 {
@@ -30,14 +41,26 @@ int	sync_status(int status)
 	return (status);
 }
 
-int	is_special_buildin(t_env *mini_env, t_shell *command)
+t_leak	*nice_little_trash(t_env *mini_env, char **path, t_shell *command)
+{
+	t_leak	*garbage;
+
+	garbage = ft_calloc(1, sizeof(t_leak));
+	if (!garbage)
+		exit_program(mini_env, 2);
+	garbage->path = path;
+	garbage->child_id = init_pid_array(mini_env, size_t_shell(command));
+	return (garbage);
+}
+
+int	is_special_buildin(t_env *mini_env, t_shell *command, t_leak *garbage)
 {
 	if (!ft_strncmp(command->command->str, "exit", 5))
 	{
 		if (command->output == 3)
 			return (0);
 		else
-			ft_exit(mini_env, 0);
+			ft_exit(mini_env, garbage);
 	}
 	if (!ft_strncmp(command->command->str, "export", 7))
 		ft_export(mini_env, command);

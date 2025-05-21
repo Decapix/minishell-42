ed53b32184ceac397a7c4f3f8f39149901daa9e8
Jean-Baptiste Lepany <jlepany@f1r5s15.clusters.42paris.fr>
2025-05-21 16:52:03 +0200

    added garbage collector for correct exit inside child and others

--- commands/commands.h
+++ commands/commands.h
@@ -6,7 +6,7 @@
 /*   By: jlepany <jlepany@student.42,fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 11:51:31 by jlepany           #+#    #+#             */
-/*   Updated: 2025/05/16 14:13:15 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/21 16:15:39 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,14 @@ void	*ft_calloc(size_t nb, size_t size);
 int		ft_strlen(char *str);
 void	exit_program(t_env *mini_env, int error_code);
 void	free_double_char(char **to_free);
+int		clean_garbage(t_leak *garbage);
 
 void	ft_lstclear(t_list **lst);
 void	ft_lstadd_back(t_list **lst, t_list *neww);
 t_list	*ft_lstnew(char *str);
 char	*join_var_equals_value(char *var, char *val);
 
-int		ft_exit(t_env *mini_env, char **env);
+int		ft_exit(t_env *mini_env, t_leak *garbage);
 int		ft_env(t_env *mini_env);
 int		ft_echo(char **str);
 int		ft_pwd(void);
--- commands/exit.c
+++ commands/exit.c
@@ -6,15 +6,15 @@
 /*   By: jlepany <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 14:00:58 by jlepany           #+#    #+#             */
-/*   Updated: 2025/05/21 14:19:55 by jlepany          ###   ########.fr       */
+/*   Updated: 2025/05/21 16:15:46 by jlepany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands.h"
 
-int	ft_exit(t_env *mini_env, char **envp)
+int	ft_exit(t_env *mini_env, t_leak *garbage)
 {
-	free_double_char(envp);
+	clean_garbage(garbage);
 	exit_program(mini_env, 0);
 	return (1);
 }
